f059611337991cef31057bc05da316bac49ab4b6
Mabe0116 <115063548+Mabe0116@users.noreply.github.com>
2024-07-11 15:03:51 +0900

    再度エラー

--- Assets/FloorScript.cs
+++ Assets/FloorScript.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FloorScript : MonoBehaviour
-{Å@
+{
+    ReflectionProbe probe;
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.probe = GetComponent<ReflectionProbe>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //yÇ…1ÇÇ©ÇØÇƒãtë§Ç…îzíu
+        this.probe.transform.position =
+        new Vector3(Camera.main.transform.position.x,
+                    Camera.main.transform.position.y * -1,
+                    Camera.main.transform.position.z);
+        probe.RenderProbe();
     }
 }
